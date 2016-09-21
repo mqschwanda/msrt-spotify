94cbf2d562f473308885e9378c90e827c50536ee
Mark Q Schwanda <MQSchwanda@ODOs-MacBook-Pro.local>
2016-09-21 17:17:26 -0400

    finalized template for group to pull and start working

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,14 +1,47 @@
-var search;
+var search, returnSong, topResult, returnResult, songTitle, songAlbum, songAlbumImg, songArtist;
 
 // function that calls Spotify API from search
 function spotifyAPI() {
-  console.log(search);
+  //
+  // place Spotify call function here
+  //
+
+  // flag for no result
+  if (typeof(result) !== 'undefined') {
+    // flag that a result has been returned
+    returnResult = true;
+  } else {
+    returnResult = false;
+  }
 }
 
 // updates search bar drop-down with top result
-function topResult(){
-  var topResult = search;
-  $('#top-result').html(topResult);
+function setTopResult(){
+  songTitle = "Song Title: topResult"; // Replace this with variables from the Spotify API object
+  songAlbum = "Song Album: topResult"; // Replace this with variables from the Spotify API object
+  songAlbumImg = "Song Album Img: topResult"; // Replace this with variables from the Spotify API object
+  songArtist = "Song Artist: topResult"; // Replace this with variables from the Spotify API object
+  // set topRestult
+  topResult = {
+    'title': songTitle,
+    'album': songAlbum,
+    'albumImg': songAlbumImg,
+    'artist': songArtist
+  };
+  // reset flag
+  returnSong = false;
+}
+
+function printTopResult(){
+  // determine if there is a song to show or search should be shown
+  if (returnSong === false) {
+    $('#song-title').html("Song Title: " + search);
+  } else {
+    $('#song-title').html(topResult.title);
+    $('#song-album').html(topResult.album);
+    $('#album-img').attr('src', topResult.albumImg);
+    $('#song-artist').html(topResult.artist);
+  }
 }
 
 // print into Watson pane
@@ -29,7 +62,8 @@ $('#sign-in').on('click', function(){
 $('#search').keyup(function(){
     search = $(this).val();
     spotifyAPI();
-    topResult();
+    setTopResult();
+    printTopResult();
 });
 
 // change color of spyglass in search bar
