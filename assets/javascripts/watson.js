f3579b6b8a619a1a58e35c3b679f146900bf5c2e
Ryan Seery <Ryan Seery>
2016-09-29 14:32:40 -0400

    referenced sentimood

--- assets/javascripts/watson.js
+++ assets/javascripts/watson.js
@@ -1,61 +1,75 @@
-/*function polarityPlaylistSearch(){
-  getUserPlaylistSongs("3ekUHhJ6QWQ6tM0KHO525Y")
-   
+var songLyricsForWatson = [];
+var watsonQuery = false;
 
-   setTimeout(function(){
-    console.log(currentPlaylistSongObjects)
+function polarityPlaylistSearch(playlistIDforWatson){
 
-    for(var i = 0; i < currentPlaylistSongObjects.length; i++){
-
-      musixTrackId = "";
-
-      var currentSongName = currentPlaylistSongObjects[i].songName;
-      var currentArtistName = currentPlaylistSongObjects[i].artistName;
-
-    
-      var currentAlbumName = "";
-      queryMusixForId(currentArtistName, currentSongName, "")
-       
-
-       /*break out after 5 sec to be safe*/
-      //setTimeout(function(){ 
-      //while(musixTrackId == ""){
-        /*Do nothing*/
-      //}
-      //musixTrackId == "broken" 
-    //}, 5000);
-
-
-       //console.log(currentSongName)
+  // Resets in case it is called more than 1 time...
+  watsonQuery = true;
+  songLyricsForWatson = [];
   
-  //}
+  // AJAX call in Spotify that uses "async: false" to wait until its done to proceed
+  getUserPlaylistSongs(playlistIDforWatson);
 
-   // }, 1000); // end compare timer
+  // Loop through all the songs and get their lyrics
+  for(var j = 0; j < currentPlaylistSongObjects.length; j++){
 
-//}*/
+    var currentSongName = currentPlaylistSongObjects[j].songName;
+    var currentArtistName = currentPlaylistSongObjects[j].artistName;
+
+    // AJAX calls in Musix that use "async: false" to wait until done to proceed
+    queryMusixForId(currentArtistName, currentSongName, "");
+
+    // Push current song lyrics to Watson list (musixLyrics is global at each increment of j)
+    songLyricsForWatson.push(musixLyrics);
+  }
+
+  // Resets in case it is called more than 1 time...
+  watsonQuery = false;
+
+  console.log(songLyricsForWatson)
+}
+
+// Ryan, u will need to pass in your own spotify playlist id here... (log in to ur spotify account and right click a playlist to get one... also dont forget to update the first 2 lines of spotify.js with ur own spotifyId)
+  polarityPlaylistSearch("3ekUHhJ6QWQ6tM0KHO525Y"); 
+
+// rest assured the code works though, if u cant get a result here, then comment out line 33 and use the below array as a data sample to make your emotion calls
+// again the key for the AJAX to wait is... "$.ajax({url:  queryURL, async : false, method: 'GET'}).done(function(response){" ... use that for ur call to the other API
+sampleWatsonLyricsResult = ["See me comin' to town with my soul Straight down …s Broke down out in a ditch of old rubbish ...  ", "I heard sirens in my head From the first time tha…opes up Cause then I'll never get let down ...  ", "So if you're lonely, you know I'm here waiting fo…You don't know You say, you don't go ...  ", "Giddy up, giddy it up Giddy up, giddy up  Wanna m…our life Live it once, can't live it twice ...  ", "I've been alone with you inside my mind And in my… what to do And I want to tell you so much ...  "];
+console.log(sampleWatsonLyricsResult)
+
+sentiment = Sentimood();
+var analyze = sentiment.analyze(sampleWatsonLyricsResult),
+  positivity = sentiment.positivity(),
+  negativity = sentiment.negativity();
+
+  analyze(sampleWatsonLyricsResult);
+
+
+
+
 
 //Running the playlist lyrics through the Text Gain API
-var polarity;
+/*var polarity;
 var polarityResponse;
-function polarityPlaylistSearch(){
-  var queryURL = "https://api.textgain.com/1/sentiment/?q=" + musixLyrics;
-  $.ajax({url: queryURL, method: 'GET'})
+function watsonPlaylistSearch(){
+  var queryURL = "https://api.textgain.com/1/sentiment/?q=" + polarityPlaylistSearch;
+  $.ajax({url: queryURL, async : false, method: 'GET'})
    .done(function(musicPolarityResponse){
      console.log(queryURL);
      console.log(musicPolarityResponse);
 
      //Globally store the polarity
-     polarityResponse = polarity;
+     //polarityResponse = polarity;
 
      //Collect the polarity and confidence
-     polarity = musicPolarityResponse.polarity.confidence;
+     //polarity = musicPolarityResponse.polarity.confidence;
 
      //Initiate next AJAX call
-     polarityPlaylistSearch(musixLyrics);
+     watsonPlaylistSearch(sampleWatsonLyricsResult);
     });
 }
 
-//Running the current song through the Text Gain API
+Running the current song through the Text Gain API
 function currentSongPolarity(){
 var queryURL = "https://api.textgain.com/1/sentiment/?q=" + musixLyrics;
   $.ajax({url: queryURL, method: 'GET'})
@@ -72,9 +86,9 @@ var queryURL = "https://api.textgain.com/1/sentiment/?q=" + musixLyrics;
 } 
 
 
-polarityPlaylistSearch();
-currentSongPolarity();
-
+watsonPlaylistSearch();*/
+/*currentSongPolarity();
+*/
 
 
 
